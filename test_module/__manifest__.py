95b948a08c52cb200d10ed14cfabaebb4f2292ed
ludlaf <ldlafaurie@gmail.com>
2021-08-15 20:07:56 +0200

    add right info to init dot py and manifest dot py

--- test_module/__manifest__.py
+++ test_module/__manifest__.py
@@ -1,3 +1,20 @@
-  {
-  
-  }
+{
+  'name': 'Test module',
+  'depends': [
+    'account',
+  ],
+  'author': 'Luis Laf',
+  'summary': 'Module for testing Odoo v14',
+  'description': """
+    This is an extended description for the module that is testing our
+    ~~skills~~ developing in Odoo. With this we'll learn:
+
+    * Basic module structure
+    * How to install modules
+    * Other things... --10815
+  """,
+  'license': 'AGPL-3',
+  'website': 'https://www.tecnativa.com',
+  'version': '14.0.1.0.0',
+  'category': 'Tools',
+}
