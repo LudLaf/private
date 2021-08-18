eef8fa6a3cf3be47b5c0c22dee6ca65e2855d0d0
ludlaf <ldlafaurie@gmail.com>
2021-08-18 22:35:59 +0200

    deleted *version* from summary in manifest file | 2nd try

--- test_module/__manifest__.py
+++ test_module/__manifest__.py
@@ -4,14 +4,14 @@
     'account',
   ],
   'author': 'Luis Laf',
-  'summary': 'Module for testing Odoo v14',
+  'summary': 'Module for testing Odoo',
   'description': """
     This is an extended description for the module that is testing our
     ~~skills~~ developing in Odoo. With this we'll learn:
 
     * Basic module structure
     * How to install modules
-    * Other things... --10815
+    * Other things... --10818
   """,
   'license': 'AGPL-3',
   'website': 'https://www.tecnativa.com',
